0a164f7745214261a0b6609890661d52a2703ac2
normanrs <normanrs@live.com>
2018-08-22 18:44:45 -0600

    No errors but nonworking sort

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Deck
   attr_reader :cards
 
@@ -7,5 +9,31 @@ class Deck
 
   def count
     cards.count
+    # binding.pry
+  end
+
+  def sort
+    value_rank = {"2" => 1, "3" => 5, "4" => 9, "5" => 13,
+                "6" => 17, "7" => 21, "8" => 25, "9" => 29,
+                "10" => 33, "Jack" => 37, "Queen" => 41,
+                "King" => 45, "Ace" => 49}
+
+    suit_rank = {"Clubs" => 0, "Diamonds" => 1, "Hearts" => 2, "Spades" => 3}
+    sort_done = false
+    sorted = []
+    until sort_done == true
+      (cards.count - 1).times do
+      # cards.each_with_index do |card, place|
+      place = 0
+        sort_done = true
+        if (value_rank[cards[place].value] + suit_rank[cards[place].suit]) > (value_rank[cards[place + 1].value] + suit_rank[cards[place + 1].suit])
+          sort_done = false
+          cards.insert(place + 1, cards[place])
+          cards.delete_at(place)
+        end
+      place = place + 1
+      end
+    end
+    cards
   end
 end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -1,6 +1,7 @@
 require 'pry'
 require 'minitest/autorun'
 require 'minitest/pride'
+require './lib/card.rb'
 require './lib/deck.rb'
 
 class DeckTest < Minitest::Test
@@ -32,6 +33,17 @@ class DeckTest < Minitest::Test
     assert_equal expected, actual
   end
 
+  def test_it_can_sort_the_deck
+    card_1 = Card.new("4", "Hearts")
+    card_2 = Card.new("Jack", "Clubs")
+    card_3 = Card.new("5", "Diamonds")
+    card_4 = Card.new("Ace", "Spades")
+    card_5 = Card.new("Ace", "Diamonds")
+    deck = Deck.new([card_1, card_2, card_3, card_4, card_5])
+    actual = deck.sort
+    expected = [card_1, card_3, card_2, card_5, card_4]
+    assert_equal expected, actual
+  end
 
 
 end
